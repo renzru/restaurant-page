218b308cb0a2c75fb5c507dd100c5065774f60ab
Renz Landicho <renzrulandicho@gmail.com>
2022-12-31 13:09:01 +0800

    add: function for creating cover card

--- src/home.ts
+++ src/home.ts
@@ -1,14 +1,15 @@
 import './normalize.css'
 import './style.css'
+import * as data from './products.json'
 import { createImage, createText } from './utils';
 
 
 function getHome() {
     const main = document.createElement('main');
-
     const cover = getCover();
+    const coverCard = getCoverCard();
 
-    main.append(cover);
+    main.append(cover, coverCard);
     return main;
 }
 
@@ -45,6 +46,49 @@ function getCoverPriceTag() {
     priceTag.append(priceText, priceValue);
 
     return priceTag;
+}
+
+function getCoverCard() {
+    const coverCard = document.createElement('section');    
+    const container = document.createElement('article');
+
+    const header = createText('h1', 'Crispy Honey-Garlic Chicken $8.99');
+    const body = getCoverCardBody();
+    const actions = getCoverCardActions();
+
+    coverCard.classList.add('cards-container--cover');
+    container.classList.add('card', 'card--cover');
+    header.classList.add('card__header');
+    
+    container.append(header, body, actions);
+    coverCard.append(container);
+
+    return coverCard;
+}
+
+function getCoverCardActions() {
+    const actions = document.createElement('div');
+    const button = createText('button', 'Add to Basket');
+
+    actions.classList.add('card__actions');
+    button.classList.add('card__purchase');
+
+    actions.append(button);
+    return actions;
+}
+
+function getCoverCardBody() {
+    const products = data;
+    const body = document.createElement('div');
+    const divider = document.createElement('hr');
+    const description = createText('p', products.CHG_chicken.description);
+    
+    body.classList.add('card__body');
+    divider.classList.add('card__divider');
+    description.classList.add('card__description');
+
+    body.append(divider, description);
+    return body;
 }
 
 export {getHome};
