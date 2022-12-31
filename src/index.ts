223b8c3af3a65b34e6a7ea3445359fb41ce5840e
Renz Landicho <renzrulandicho@gmail.com>
2022-12-31 10:58:52 +0800

    add: module for loading nav

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-import './style.css'
-import './normalize.css'
-
-const btn = document.createElement('button')
-btn.classList.add('content')
-
-document.body.appendChild(btn)
+import { startSite } from "./site";
+startSite();
